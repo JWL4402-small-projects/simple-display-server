2fbef80f907b714b6b46d58581854ee45943e93f
JWL <j.lebert4402@gmail.com>
2023-02-17 15:18:27 -0500

    serves an index.html

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 from threading import Thread
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return "Hello world!"
+	return render_template('index.html')
 
 def run():
 	app.run(host="0.0.0.0", port=8080)
